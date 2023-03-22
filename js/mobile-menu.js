d0c8a7ebec476de9747281cb8e0d2385c5b07f6e
MaksymBora <maxboraod@gmail.com>
2023-03-22 19:55:44 +0100

    added mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // menu click event
+  $('.menuBtn').click(function () {
+    $(this).toggleClass('act');
+    if ($(this).hasClass('act')) {
+      $('.mainMenu').addClass('act');
+    } else {
+      $('.mainMenu').removeClass('act');
+    }
+  });
+});
